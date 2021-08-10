a0a7e54a207acad81ac606f2cef06d7e3ef4c73e
Mariana Carolina de Souza <smcarolina42@gmail.com>
2021-08-10 17:34:09 -0300

    exercicios-aula-4

---
+++ semana1/operadores/index.js
@@ -0,0 +1,11 @@
+//EXERCICIO 1 
+//a. false
+//b. false
+//c. true
+//d. booolean
+
+//EXERCICIO 2
+//Este programa tenta somar dois números fornecidos pelo usuário. Porém, o retorno de um prompt é, por padrão, uma string, não sendo possível realizar a soma. 
+//Solução: É preciso transformar a string recebida no prompt em number.
+// let primeiroNumero = Number(prompt("Digite um numero!"))
+// let segundoNumero = Number(prompt("Digite outro numero!"))
