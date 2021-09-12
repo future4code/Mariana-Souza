16f9e93f09b262493c5e754f3d724e56cc6da54e
Mariana Carolina de Souza <smcarolina42@gmail.com>
2021-09-12 13:47:11 -0300

    estilização do projeto

--- semana5/projeto-whatslab/whatslab/src/App.js
+++ semana5/projeto-whatslab/whatslab/src/App.js
@@ -5,20 +5,66 @@ const PageContainer = styled.div`
   border: 1px solid black;
   box-sizing: border-box;
   height: 100vh;
-  width: 50vw;
+  width: 30vw;
   margin: auto;
   display: flex;
   flex-direction: column;
+  background-color: #faebd7;
 `
 
 const Mensagens = styled.div`
   flex-grow: 1;
   display: flex;
   flex-direction: column-reverse;
+  
 `
 const Inputs = styled.div`
   display: grid;
-  grid-template-columns: 1fr 4fr 1fr;
+  grid-template-columns: 20% auto 10%;
+  column-gap: .5em;
+  padding: 0 0.5em;
+`
+const CaixaInput = styled.input `
+  outline: none;
+  border: none;
+  border-radius: 0.5em;
+  height: 2rem;
+  padding: 0.2em;
+  font-size: 1.2em;
+`
+const Botao = styled.button `
+  background-color: transparent;
+  border: none;
+`
+
+const BalaoMensagemEu = styled.div `
+  background-color: #DDF7C8;
+  margin: .5em;
+  padding: .5em 1em;
+  border-radius: .5em;
+  align-self: flex-end;
+  box-shadow: 0px 3px 3px 0px rgba(0, 0, 0, 0.2);
+  max-width: 60%;
+  min-width: 8%;
+  min-height: 1.4em;
+  word-wrap: break-word;
+`
+
+const BalaoMensagemOutro = styled.div `
+  background-color: #fff;
+  margin: .5em;
+  padding: 0 1em;
+  border-radius: .5em;
+  align-self: flex-start;
+  box-shadow: 0px 3px 3px 0px rgba(0, 0, 0, 0.2);
+  max-width: 60%;
+  min-width: 8%;
+  word-wrap: break-word;
+`
+
+const ImagemBotao = styled.img`
+  width: 60%;
+
 `
 class App extends React.Component {
   state = {
@@ -54,10 +100,24 @@ class App extends React.Component {
     return (
       <PageContainer>
         <Mensagens>
+          
+          
           {this.state.mensagens.map((mensagem, index) => {
-            return <p key={index}>
-              <strong>{mensagem.usuario}</strong>: {mensagem.mensagem}
-            </p>
+            const nome = mensagem.usuario.toLowerCase()
+           
+            if(nome === 'eu'){
+              return <BalaoMensagemEu>
+                <p key={index}>
+                  {mensagem.mensagem}
+                </p>
+                </BalaoMensagemEu>
+            }
+            else{
+              return <BalaoMensagemOutro>
+                  <p key={index}><strong>{mensagem.usuario}</strong></p>
+                  <p key={index}>{mensagem.mensagem}</p>
+                </BalaoMensagemOutro>
+            }
           })
 
           }
@@ -65,17 +125,19 @@ class App extends React.Component {
 
         </Mensagens>
         <Inputs>
-          <input
+          <CaixaInput
             onChange={this.onChangeValorUsuario}
             value={this.state.valorUsuario}
             placeholder={'usuário'}
           />
-          <input
+          <CaixaInput
             onChange={this.onChangeValorMensagem}
             value={this.state.valorMensagem}
             placeholder={'mensagem'}
           />
-          <button onClick={this.enviaMensagem}>Enviar</button>
+          <Botao onClick={this.enviaMensagem}>
+            <ImagemBotao src="https://image.flaticon.com/icons/png/512/786/786205.png"/>
+          </Botao>
         </Inputs>
       </PageContainer>
     );
