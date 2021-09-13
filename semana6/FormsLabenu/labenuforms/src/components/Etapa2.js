f31b1af4e486dc6ee88b79492654a2f1fc9a5367
Mariana Carolina de Souza <smcarolina42@gmail.com>
2021-09-13 20:32:58 -0300

    exercicio-aula21

---
+++ semana6/FormsLabenu/labenuforms/src/components/Etapa2.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default class Etapa2 extends React.Component {
+    render() {
+        return (
+            <div>
+                <h2>Etapa 2</h2>
+                <label>Qual o curso?</label>
+                <input />
+                <label>Qual a unidade de ensino?</label>
+                <input />
+            </div>
+        );
+    }
+
+}
+
+
