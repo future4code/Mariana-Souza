595b79efb4a12f2ba689b2a6a55b8f5341c37e73
Mariana Carolina de Souza <smcarolina42@gmail.com>
2021-11-25 18:56:08 -0300

    exercicio aula 47

--- semana16/knex/src/connection.ts
+++ semana16/knex/src/connection.ts
@@ -6,11 +6,11 @@ dotenv.config();
 const connection = knex({
    client: "mysql",
    connection: {
-      host: process.env.DB_HOST,
+      host: "35.226.146.116",
       port: 3306,
-      user: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_SCHEMA,
+      user: "mariana-carolina",
+      password: "oTX!TpCP0xn8m!sX9LyD",
+      database: "maryam-mariana-carolina",
       multipleStatements: true
    },
 });
